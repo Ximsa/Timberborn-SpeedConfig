706b14aad46db43707a0b35e6349ae740c7a0880
Ximsa <burrfelix@aol.de>
2025-11-13 17:20:23 +0100

    fix for 1.0

--- Plugin.cs
+++ Plugin.cs
@@ -4,9 +4,9 @@ using Timberborn.ModManagerScene;
 
 namespace SpeedConfig
 {
-    public class Plugin: IModStarter
+    public class Plugin : IModStarter
     {
-        public void StartMod()
+        public void StartMod(IModEnvironment modEnvironment)
         {
             Console.WriteLine("Hello SpeedConfig!");
             Harmony harmony = new Harmony("speedconfig");
